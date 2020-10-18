e8855c27311c79be41c996e423b5d11ff94f86c0
Julian_Glass-Pilon <jglasspilon@live.com>
2020-10-18 14:26:59 -0400

    Added some animations to the DataBinderDemo scene elements and some visual polish

--- Assets/DataBinder/DataBinderDemo.cs
+++ Assets/DataBinder/DataBinderDemo.cs
@@ -18,10 +18,23 @@ public class DataBinderDemo : MonoBehaviour
     private Animator m_chartWipe;                               //Animator that controls the animation of the wipe that covers the chart during data transition
 
     private const string PATH_TO_JSON_FILE = "/JSONData/";      //Path (relative to the streaming assets path) pointing to the JSON files
+    private const string STARTUP_DATA = "Nasdaq.json";          //Specifies the JSON file to read from on app start
 
     private void Start()
     {
-        TryChangeData("NYSE.json");
+        SetStartupData();
+    }
+
+    /// <summary>
+    /// Read from the startup data json file and bind the data to the dataBinder and dataBinder list, bypassing the wipe animation
+    /// </summary>
+    private void SetStartupData()
+    {
+        JSONNode json = FileReader.ReadJSONFromFile(Application.streamingAssetsPath + PATH_TO_JSON_FILE + STARTUP_DATA);
+        if (json != null)
+            StartCoroutine(ChangeData(json, false));
+        else
+            Debug.LogError($"JSON file {STARTUP_DATA} does not exist. Could not change data.");
     }
 
     /// <summary>
@@ -32,10 +45,9 @@ public class DataBinderDemo : MonoBehaviour
     {
         JSONNode json = FileReader.ReadJSONFromFile(Application.streamingAssetsPath + PATH_TO_JSON_FILE + JSONFile);
         if (json != null)
-        {
-            Debug.Log(json["ChartData"].ToString());
             StartCoroutine(ChangeData(json));
-        }
+        else
+            Debug.LogError($"JSON file {JSONFile} does not exist. Could not change data.");
     }
 
     /// <summary>
@@ -44,16 +56,17 @@ public class DataBinderDemo : MonoBehaviour
     /// </summary>
     /// <param name="json">JSON data to pass to the dataBinder elements</param>
     /// <returns></returns>
-    private IEnumerator ChangeData(JSONNode json)
+    private IEnumerator ChangeData(JSONNode json, bool playAnimation = true)
     {
         //Generates a list of dataBinders based on the json provided
-        m_dataList.GenerateListInstant(json);
+        m_dataList.GenerateList(json);
 
         //Registers the json data provided to the dataBinder
         m_dataBinder.RegisterData(json);
 
         //Plays the animation for the chart wipe and waits for half of the animation before yielding
-        yield return StartCoroutine(AnimationDispatcher.TriggerAnimation(m_chartWipe, "Play", 0.5f));
+        if(playAnimation)
+            yield return StartCoroutine(AnimationDispatcher.TriggerAnimation(m_chartWipe, "Play", 0.5f));
 
         //Bind the new data to all databinder components tied to the dataBinder
         m_dataBinder.BindData(); 
